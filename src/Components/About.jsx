d7f48d3ae5586bbdc10393dfd162f3c4997a0acf
Jeoffrey <jeoff.howe@gmail.com>
2019-09-18 22:19:15 -0600

    about added

---
+++ src/Components/About.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class About extends Component {
+    constructor() {
+        super();
+        this.state = {
+            admin: false,
+            information: ''
+        }
+    }
+return () {
+    render(<div>
+        {DisplayInformation}
+    </div>)
+}
+}
+export default About
