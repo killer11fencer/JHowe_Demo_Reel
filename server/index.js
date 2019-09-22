593d88835153dc456c77e3130fb4c942d3bc6945
Jeoffrey <jeoff.howe@gmail.com>
2019-09-21 23:30:53 -0600

    added server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+express = require('express')
+session = require('express-session')
+massive = require('massive')
+const {CONNECTION_STRING,SERVER_PORT,SESSION_SECRET} = process.env
+
+app = express()
+
+app.use(express.json())
+
+app.use(
+    session({
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+      }
+    })
+  );
+  
+massive(CONNECTION_STRING).then(db=>{
+    app.set('db',db);
+    console.log(db.listTables())
+    console.log('DATABASE: Connected')
+    app.listen(SERVER_PORT, () => console.log('listening on server',SERVER_PORT))
+}).catch(err => console.log('err on port',err))
