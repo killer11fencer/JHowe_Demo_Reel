b85dc28935a9029548b7312cf9d1470923bb9253
Jeoffrey <jeoff.howe@gmail.com>
2019-09-21 23:40:15 -0600

    controller

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 express = require('express')
 session = require('express-session')
 massive = require('massive')
+const video_Ctrl = require('./controller/video_Ctrl')
 const {CONNECTION_STRING,SERVER_PORT,SESSION_SECRET} = process.env
 
 app = express()
@@ -18,10 +19,13 @@ app.use(
       }
     })
   );
-  
+
 massive(CONNECTION_STRING).then(db=>{
     app.set('db',db);
     console.log(db.listTables())
     console.log('DATABASE: Connected')
     app.listen(SERVER_PORT, () => console.log('listening on server',SERVER_PORT))
 }).catch(err => console.log('err on port',err))
+
+app.get('/api/videos',video_Ctrl.get_videos)
+app.post('/api/videos',video_Ctrl.add_video)
