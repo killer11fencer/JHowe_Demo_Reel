31678f6b2bfeca05cbf7235d769c0539450dc4b0
Jeoffrey <jeoff.howe@gmail.com>
2019-10-11 23:37:23 -0600

    added map

--- src/Components/Referrals.jsx
+++ src/Components/Referrals.jsx
@@ -15,7 +15,23 @@ get_referrals = () => {
     axios.get('api/referrals').then( result => {this.setState({referrals: result})})
 }
 render() {
-    return(<div>
+    let display_referrals = this.state.referrals.map((referral,id) => {
+        return <div id={id}>
+            <h4>Date Submitted</h4>
+            <h5>{referral.date_created}</h5>
+            <h4>First Name</h4>
+            <h5>{referral.first_name}</h5>
+            <h4>Last Name</h4>
+            <h5>{referral.last_name}</h5>
+            <h4>Phone Number</h4>
+            <h5>{referral.phone}</h5>
+            <h4>Email</h4>
+            <h5>{referral.email}</h5>
+            <h4>Project Details</h4>
+            <h5>{referral.project}</h5>
+        </div>
+    })
+    return( <div>
             {display_referrals}
     </div>)
 }
