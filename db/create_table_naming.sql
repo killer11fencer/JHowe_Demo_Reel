53c299179744c5cb460fdefcb6e780ede2139a97
Jeoffrey <jeoff.howe@gmail.com>
2019-12-01 22:34:35 -0700

    creating database

--- db/create_table_naming.sql
+++ db/create_table_naming.sql
@@ -9,10 +9,22 @@ create table nc.users (
 create table nc.department (
     id serial NOT NULL,
     department_name varchar,
-    department_id varchar
+    department_abbr varchar,
+    description_id integer,
 )
 
-create table nc.file_name (
+create table nc.description (
+    id serial NOT NULL,
+    description varchar(255)
+)
+create table nc.versions (
     id serial NOT NULL,
     
+)
+create table nc.file_name (
+    id serial NOT NULL,
+    department_id integer,
+    description_id integer,
+    inserted_at timestamp WITH time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at  timestamp WITH time zone DEFAULT CURRENT_TIMESTAMP NOT NULL  
 )
